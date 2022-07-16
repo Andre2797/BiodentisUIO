f5be33dc97f473b6f198cd64046bbe8d1d12c878
Andrea Silva <55775461+Andre2797@users.noreply.github.com>
2022-07-16 14:10:44 -0500

    cedula cambios

--- src/app/components/historia-clinica/historia-clinica.component.ts
+++ src/app/components/historia-clinica/historia-clinica.component.ts
@@ -288,46 +288,38 @@ export class HistoriaClinicaComponent implements OnInit {
   goBack() {
     this.router.navigate(['/menu/home']);
   }
-  public validador; 
+  public validador;
   validadorDeCedula() {
-    console.log("ENTRAAAAA")
-    let cedulaCorrecta = false;
-    let cedula=(<HTMLInputElement>document.getElementById('cedula')).value
-    console.log(cedula)
-    if (cedula.length == 10)
-    {    
-        let tercerDigito = parseInt(cedula.substring(2, 3));
-        if (tercerDigito < 6) {
-            // El ultimo digito se lo considera dígito verificador
-            let coefValCedula = [2, 1, 2, 1, 2, 1, 2, 1, 2];       
-            let verificador = parseInt(cedula.substring(9, 10));
-            let suma:number = 0;
-            let digito:number = 0;
-            for (let i = 0; i < (cedula.length - 1); i++) {
-                digito = parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];      
-                suma += ((parseInt((digito % 10)+'') + (parseInt((digito / 10)+''))));
-          //      console.log(suma+" suma"+coefValCedula[i]); 
-            }
-            suma= Math.round(suma);
-          //  console.log(verificador);
-          //  console.log(suma);
-          //  console.log(digito);
-            if ((Math.round(suma % 10) == 0) && (Math.round(suma % 10)== verificador)) {
-                cedulaCorrecta = true;
-            } else if ((10 - (Math.round(suma % 10))) == verificador) {
-                cedulaCorrecta = true;
-            } else {
-                cedulaCorrecta = false;
-            }
+
+    var cad: any = String((<HTMLInputElement>document.getElementById('cedula')).value.trim());
+    var total = 0;
+    var longitud = cad.length;
+    var longcheck = longitud - 1;
+
+    if (cad !== "" && longitud === 10) {
+
+      for (let i = 0; i < longcheck; i++) {
+        if (i % 2 === 0) {
+          var aux = cad.charAt(i) * 2;
+          if (aux > 9) aux -= 9;
+          total += aux;
         } else {
-            cedulaCorrecta = false;
+          total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
         }
-    } else {
-        cedulaCorrecta = false;
+      }
+
+      total = total % 10 ? 10 - total % 10 : 0;
+
+      if (cad.charAt(longitud - 1) == total) {
+        this.validador = false
+      } else {
+        this.validador = true
+      }
     }
-    console.log("validadorrrrr",this.validador)
-  this.validador= cedulaCorrecta;
-  
+    console.log(this.validador)
   }
+
+
+
 
 }
